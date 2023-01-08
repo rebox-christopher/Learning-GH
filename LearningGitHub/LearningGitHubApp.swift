aa8b9957cc40e0611a8e1f46bbd987a818e9684a
rebox-christopher <chris.roberts.ames@gmail.com>
2023-01-08 16:35:07 -0600

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Chris Roberts on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
